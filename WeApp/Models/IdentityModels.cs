72c7383c62fabbb084ee91aa5b8768b0e9fde68d
3x0dv5 <eng.ruilima@gmail.com>
2014-09-14 19:41:46 +0100

    First commit
    First commit

---
+++ WeApp/Models/IdentityModels.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentitySample.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser<string, ApplicationUserLogin, 
+                                   ApplicationUserRole, ApplicationUserClaim>
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationRole : IdentityRole<string, ApplicationUserRole>
+    {
+        public ApplicationRole()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public ApplicationRole(string name):this()
+        {
+            this.Name = name;
+        }
+
+        // Add Any custom Role properties/code here
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, 
+                                         string, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        static ApplicationDbContext()
+        {
+            // Set the database intializer which is run once during application start
+            // This seeds the database with admin user credentials and admin role
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+
+    public class ApplicationUserStore :
+        UserStore <ApplicationUser, ApplicationRole, string, ApplicationUserLogin, ApplicationUserRole, ApplicationUserClaim>,
+        IUserStore<ApplicationUser, string>,
+        IDisposable
+    {
+        public ApplicationUserStore() : this(new IdentityDbContext())
+        {
+            base.DisposeContext = true;
+        }
+
+        public ApplicationUserStore(DbContext context):base(context)
+        {
+            
+        }
+    }
+}
